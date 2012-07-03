8b3cd76dca6780d3f52d10f899b1b7f03293d72c
JeanMarcDENIEL <jean-marc.deniel@inrs.fr>
2012-07-03 12:11:29 +0200

    New class added

---
+++ C4Sci/src/main/java/c4sci/math/Floatings.java
@@ -0,0 +1,34 @@
+package c4sci.math;
+
+/**
+ * Useful floating operations managing uncertainty.
+ * @author jeanmarc.deniel
+ *
+ */
+public final class Floatings {
+
+	private Floatings(){}
+	
+	public static final float	EPSILON_VALUE = 0.0001f;
+	
+	public static boolean isLess(float first_term, float second_term){
+		return first_term-second_term < -EPSILON_VALUE;
+	}
+	
+	public static boolean isEqual(float first_term, float second_term){
+		return Math.abs(first_term - second_term) < EPSILON_VALUE;
+	}
+	
+	public static boolean isMore(float first_term, float second_term){
+
+		return second_term - first_term > EPSILON_VALUE;
+	}
+	
+	public static boolean isLessEqual(float first_term, float second_term){
+		return isLess(first_term,  second_term) || isEqual(first_term,  second_term);
+	}
+	
+	public static boolean isMoreEqual(float first_term, float second_term){
+		return isMore(first_term, second_term) || isEqual(first_term, second_term);
+	}
+}
