fd4c486bc8b28cd98089c4dd9eaf0fb671cb0c0f
JeanMarcDENIEL <jean-marc.deniel@inrs.fr>
2012-07-06 14:23:09 +0200

    TransformMatrix methods added & tested

--- C4Sci/src/main/java/c4sci/math/geometry/space/TransformMatrix.java
+++ C4Sci/src/main/java/c4sci/math/geometry/space/TransformMatrix.java
@@ -9,14 +9,25 @@ package c4sci.math.geometry.space;
 public class TransformMatrix {
 
 	final static int NB_COOR = 4;
+	/**
+	 * 1st coor = row
+	 * 2nd coor = column
+	 */
 	private float valueTab[][];
+	
+	public void setValue(int _row, int _col, float _val){
+		valueTab[_row][_col] = _val;
+	}
+	public float getValue(int _row, int _col){
+		return valueTab[_row][_col];
+	}
 	
 	/*********** CONSTRUCTORS ***************/
 	private TransformMatrix(){
 		valueTab = new float[NB_COOR][NB_COOR];
 		for (int _i = 0; _i<NB_COOR; _i++){
 			for (int _j=0; _j<NB_COOR; _j++){
-				valueTab[_i][_j] = 0.0f;
+				setValue(_i, _j, 0.0f);
 			}
 		}
 	}
@@ -25,12 +36,46 @@ public class TransformMatrix {
 	public static final TransformMatrix createIdentityMatrix(){
 		TransformMatrix _res = new TransformMatrix();
 		for (int _i=0; _i<NB_COOR; _i++)
-			_res.valueTab[_i][_i] = 1.0f;
+			_res.setValue(_i, _i, 1.0f);
+		return _res;
+	}
+	public static final TransformMatrix createScaleMatrix(float x_scale, float y_scale, float z_scale, float w_scale){
+		TransformMatrix _res = new TransformMatrix();
+		int _x = Commons.CoorName.X.getCoorValue();
+		int _y = Commons.CoorName.Y.getCoorValue();
+		int _z = Commons.CoorName.Z.getCoorValue();
+		int _w = Commons.CoorName.W.getCoorValue();
+		_res.setValue(_x, _x, x_scale);
+		_res.setValue(_y, _y, y_scale);
+		_res.setValue(_z, _z, z_scale);
+		_res.setValue(_w, _w, w_scale);
 		return _res;
 	}
 	
 	/*********** OPERATORS ******************/
-	/*public static final SpaceVector opMul(SpaceVector _vec){
-		
-	}*/
+	public final SpaceVector opMul(SpaceVector _vec){
+		SpaceVector _res = new SpaceVector();
+		for (int _row = 0; _row<Commons.NB_COOR; _row++){
+			float _tmp = 0.0f;
+			for (int _col=0; _col<Commons.NB_COOR; _col++){
+				_tmp += _vec.getCoor(_row)*getValue(_row, _col);
+			}
+			_res.setCoor(_row, _tmp);
+		}
+		return _res;
+	}
+	
+	public final TransformMatrix opMul(TransformMatrix _mat){
+		TransformMatrix _res = new TransformMatrix();
+		for (int _row=0; _row<Commons.NB_COOR; _row++){
+			for (int _col_row_2 = 0; _col_row_2<Commons.NB_COOR; _col_row_2++){
+				float _tmp = 0.0f;
+				for (int _col_2=0; _col_2<Commons.NB_COOR; _col_2++){
+					_tmp += getValue(_row, _col_row_2) * _mat.getValue(_col_row_2, _col_2);
+				}
+				_res.setValue(_row, _col_row_2, _tmp);
+			}
+		}
+		return _res;
+	}
 }
--- C4Sci/src/test/java/c4sci/math/geometry/space/TestTransformMatrix.java
+++ C4Sci/src/test/java/c4sci/math/geometry/space/TestTransformMatrix.java
@@ -9,6 +9,18 @@ public class TestTransformMatrix {
 	@Test
 	public void testFactoryMethods() {
 		TransformMatrix _mat_1 = TransformMatrix.createIdentityMatrix();
+		SpaceVector		_vec_1 = new SpaceVector(1.0f, 2.0f, 3.0f, 1.0f);
+		assertTrue(_vec_1.isEqualTo(_mat_1.opMul(_vec_1)));
+		
+	}
+	
+	@Test
+	public void testOps(){
+		TransformMatrix _mat_1 = TransformMatrix.createIdentityMatrix();
+		SpaceVector		_vec_1 = new SpaceVector(1.0f, 2.0f, 3.0f, 1.0f);
+		TransformMatrix _mat_2 = _mat_1.opMul(TransformMatrix.createIdentityMatrix());
+		assertTrue(_vec_1.isEqualTo(_mat_2.opMul(_vec_1)));
 	}
 
 }
+
