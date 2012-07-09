6f0cf3b71e44189f676af2db4f82f6290a03d758
JeanMarcDENIEL <jean-marc.deniel@inrs.fr>
2012-07-09 09:38:53 +0200

    Shared data passed in Commons class. Coding rule compliance improved.

--- C4Sci/src/main/java/c4sci/math/geometry/space/TransformMatrix.java
+++ C4Sci/src/main/java/c4sci/math/geometry/space/TransformMatrix.java
@@ -6,40 +6,40 @@ package c4sci.math.geometry.space;
  * @author jeanmarc.deniel
  *
  */
-public class TransformMatrix {
+public final class TransformMatrix {
 
-	final static int NB_COOR = 4;
 	/**
 	 * 1st coor = row
 	 * 2nd coor = column
 	 */
 	private float valueTab[][];
 	
-	public void setValue(int _row, int _col, float _val){
-		valueTab[_row][_col] = _val;
+	public void setValue(int row_val, int col_val, float new_val){
+		valueTab[row_val][col_val] = new_val;
 	}
-	public float getValue(int _row, int _col){
-		return valueTab[_row][_col];
+	public float getValue(int row_val, int col_val){
+		return valueTab[row_val][col_val];
 	}
 	
 	/*********** CONSTRUCTORS ***************/
 	private TransformMatrix(){
-		valueTab = new float[NB_COOR][NB_COOR];
-		for (int _i = 0; _i<NB_COOR; _i++){
-			for (int _j=0; _j<NB_COOR; _j++){
+		valueTab = new float[Commons.NB_COOR][Commons.NB_COOR];
+		for (int _i = 0; _i<Commons.NB_COOR; _i++){
+			for (int _j=0; _j<Commons.NB_COOR; _j++){
 				setValue(_i, _j, 0.0f);
 			}
 		}
 	}
 	
 	/*********** FACTORY METHODS ************/
-	public static final TransformMatrix createIdentityMatrix(){
+	public static TransformMatrix createIdentityMatrix(){
 		TransformMatrix _res = new TransformMatrix();
-		for (int _i=0; _i<NB_COOR; _i++)
+		for (int _i=0; _i<Commons.NB_COOR; _i++){
 			_res.setValue(_i, _i, 1.0f);
+		}
 		return _res;
 	}
-	public static final TransformMatrix createScaleMatrix(float x_scale, float y_scale, float z_scale, float w_scale){
+	public static TransformMatrix createScaleMatrix(float x_scale, float y_scale, float z_scale, float w_scale){
 		TransformMatrix _res = new TransformMatrix();
 		int _x = Commons.CoorName.X.getCoorValue();
 		int _y = Commons.CoorName.Y.getCoorValue();
@@ -53,25 +53,25 @@ public class TransformMatrix {
 	}
 	
 	/*********** OPERATORS ******************/
-	public final SpaceVector opMul(SpaceVector _vec){
+	public SpaceVector opMul(SpaceVector other_vec){
 		SpaceVector _res = new SpaceVector();
 		for (int _row = 0; _row<Commons.NB_COOR; _row++){
 			float _tmp = 0.0f;
 			for (int _col=0; _col<Commons.NB_COOR; _col++){
-				_tmp += _vec.getCoor(_row)*getValue(_row, _col);
+				_tmp += other_vec.getCoor(_row)*getValue(_row, _col);
 			}
 			_res.setCoor(_row, _tmp);
 		}
 		return _res;
 	}
 	
-	public final TransformMatrix opMul(TransformMatrix _mat){
+	public TransformMatrix opMul(TransformMatrix other_mat){
 		TransformMatrix _res = new TransformMatrix();
 		for (int _row=0; _row<Commons.NB_COOR; _row++){
 			for (int _col_row_2 = 0; _col_row_2<Commons.NB_COOR; _col_row_2++){
 				float _tmp = 0.0f;
 				for (int _col_2=0; _col_2<Commons.NB_COOR; _col_2++){
-					_tmp += getValue(_row, _col_row_2) * _mat.getValue(_col_row_2, _col_2);
+					_tmp += getValue(_row, _col_row_2) * other_mat.getValue(_col_row_2, _col_2);
 				}
 				_res.setValue(_row, _col_row_2, _tmp);
 			}
--- C4Sci/src/test/java/c4sci/math/geometry/space/TestTransformMatrix.java
+++ C4Sci/src/test/java/c4sci/math/geometry/space/TestTransformMatrix.java
@@ -12,6 +12,11 @@ public class TestTransformMatrix {
 		SpaceVector		_vec_1 = new SpaceVector(1.0f, 2.0f, 3.0f, 1.0f);
 		assertTrue(_vec_1.isEqualTo(_mat_1.opMul(_vec_1)));
 		
+		TransformMatrix _mat_2 = TransformMatrix.createScaleMatrix(2.0f, 3.0f, 4.0f, 2.0f);
+		SpaceVector		_vec_2 = _mat_1.opMul(_mat_2).opMul(_vec_1);
+		assertTrue(_vec_2.isEqualTo(new SpaceVector(new SpaceVector(4.0f, 12.0f, 24.0f))));
+		
+		
 	}
 	
 	@Test
