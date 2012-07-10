73faf626aa082188a0e583380f6021b00e20c2d3
JeanMarcDENIEL <jean-marc.deniel@inrs.fr>
2012-07-10 15:36:51 +0200

    Improved code

--- C4Sci/src/main/java/c4sci/data/VisitableData.java
+++ C4Sci/src/main/java/c4sci/data/VisitableData.java
@@ -1,6 +1,11 @@
 package c4sci.data;
 
-
+/**
+ * This interface is intended at avoiding suspicious dependency cycling between 
+ * classes involved in a VISITOR pattern.
+ * @author jeanmarc.deniel
+ *
+ */
 public interface VisitableData {
 	void acceptVisitor(DataVisitor data_visitor);
 }
