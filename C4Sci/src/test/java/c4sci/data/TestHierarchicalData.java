b8202cd70eee67d279531ada698fd18b81725b22
JeanMarcDENIEL <jean-marc.deniel@inrs.fr>
2012-07-10 12:21:16 +0200

    Hierarchical data testing improved

---
+++ C4Sci/src/test/java/c4sci/data/TestHierarchicalData.java
@@ -0,0 +1,252 @@
+package c4sci.data;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.omg.PortableInterceptor.SUCCESSFUL;
+
+import c4sci.data.internationalization.InternationalizableTerm;
+
+public class TestHierarchicalData {
+
+	@Test
+	public void testHierarchicalData() {
+		HierarchicalData _data = new HierarchicalData("test_data", 
+				new InternationalizableTerm("testing data"), 
+				new InternationalizableTerm("this data is used to test the HierarchicalData class"));
+		assertTrue(true);
+	}
+
+	@Test
+	public void testGetDataToken() {
+		HierarchicalData _data = new HierarchicalData("test_data", 
+				new InternationalizableTerm("testing data"), 
+				new InternationalizableTerm("this data is used to test the HierarchicalData class"));
+		assertTrue("test_data".compareTo(_data.getDataToken())==0);
+
+	}
+
+	@Test
+	public void testGetDataName() {
+		HierarchicalData _data = new HierarchicalData("test_data", 
+				new InternationalizableTerm("testing data"), 
+				new InternationalizableTerm("this data is used to test the HierarchicalData class"));
+		assertTrue("testing data".compareTo(_data.getDataName().getDefaultValue())==0);
+	}
+
+	@Test
+	public void testGetDataDescription() {
+		HierarchicalData _data = new HierarchicalData("test_data", 
+				new InternationalizableTerm("testing data"), 
+				new InternationalizableTerm("this data is used to test the HierarchicalData class"));
+		assertTrue("this data is used to test the HierarchicalData class".compareTo(_data.getDataDescription().getDefaultValue())==0);
+	}
+
+	@Test
+	public void testAddDataParameter() {
+		HierarchicalData _data = new HierarchicalData("test_data", 
+				new InternationalizableTerm("testing data"), 
+				new InternationalizableTerm("this data is used to test the HierarchicalData class"));
+		DataParameter _param = new DataParameter() {
+			private int	paramValue;
+			public void setParameterValue(String str_to_parse)
+					throws DataValueParsingException {
+				try{
+					paramValue = Integer.parseInt(str_to_parse);
+				}
+				catch(NumberFormatException _e){
+					throw new DataValueParsingException("integer", str_to_parse, "integer parsing error");
+				}
+				
+			}
+			
+			public String getParameterValue() {
+				return Integer.toString(paramValue);
+			}
+			
+			public String getParameterToken() {
+				return "testParam";
+			}
+			
+			public InternationalizableTerm getParameterName() {
+				return new InternationalizableTerm("testing parameter");
+			}
+			
+			public InternationalizableTerm getParameterDescription() {
+				return new InternationalizableTerm("An integer testing parameter");
+			}
+
+		};
+
+		_data.addDataParameter(_param);
+		assertTrue(true);
+
+	}
+
+	@Test
+	public void testSetGetParameterValue() {
+		HierarchicalData _data = new HierarchicalData("test_data", 
+				new InternationalizableTerm("testing data"), 
+				new InternationalizableTerm("this data is used to test the HierarchicalData class"));
+		DataParameter _param = new DataParameter() {
+			private int	paramValue;
+			public void setParameterValue(String str_to_parse)
+					throws DataValueParsingException {
+				try{
+					paramValue = Integer.parseInt(str_to_parse);
+				}
+				catch(NumberFormatException _e){
+					throw new DataValueParsingException("integer", str_to_parse, "integer parsing error");
+				}
+				
+			}
+			
+			public String getParameterValue() {
+				return Integer.toString(paramValue);
+			}
+			
+			public String getParameterToken() {
+				return "testParam";
+			}
+			
+			public InternationalizableTerm getParameterName() {
+				return new InternationalizableTerm("testing parameter");
+			}
+			
+			public InternationalizableTerm getParameterDescription() {
+				return new InternationalizableTerm("An integer testing parameter");
+			}
+		};
+
+		_data.addDataParameter(_param);
+		
+		try {
+			_data.setParameterValue("testParam", "10");
+			assertTrue("10".compareTo(_data.getParameterValue("testParam"))==0);
+		} catch (DataValueParsingException e) {
+			fail();
+		} catch (NoSuchParameterException e) {
+			fail();
+		}
+		
+		try {
+			_data.setParameterValue("error_param", "10");
+			fail();
+		} catch (DataValueParsingException e) {
+			fail();
+		} catch (NoSuchParameterException e) {
+			assertTrue(true);
+		}
+		
+		try {
+			_data.setParameterValue("testParam", "a10");
+			fail();
+		} catch (DataValueParsingException e) {
+			assertTrue(true);
+		} catch (NoSuchParameterException e) {
+			fail();
+		}
+		
+		try {
+			String _str = _data.getParameterValue("err_str");
+			fail(_str + " should not have been read");
+		} catch (NoSuchParameterException e) {
+			assertTrue(true);
+		}
+	}
+
+
+	@Test
+	public void testAcceptVisitor() {
+		HierarchicalData _data = new HierarchicalData("test_data", 
+				new InternationalizableTerm("testing data"), 
+				new InternationalizableTerm("this data is used to test the HierarchicalData class"));
+		
+		HierarchicalData _child_1 = new HierarchicalData("child_1", 
+				new InternationalizableTerm("First child"), 
+				new InternationalizableTerm("First child description"));
+		
+		HierarchicalData _child_2 = new HierarchicalData("child_2", 
+				new InternationalizableTerm("Second child"), 
+				new InternationalizableTerm("2nd child description"));
+		
+		class TestDataParameter implements DataParameter{
+			
+			private int	paramValue;
+			String 		tokenValue;
+			
+			public TestDataParameter(String token_str){
+				tokenValue = token_str;
+			}
+			
+			public void setParameterValue(String str_to_parse)
+					throws DataValueParsingException {
+				try{
+					paramValue = Integer.parseInt(str_to_parse);
+				}
+				catch(NumberFormatException _e){
+					throw new DataValueParsingException("integer", str_to_parse, "integer parsing error");
+				}
+				
+			}
+			
+			public String getParameterValue() {
+				return Integer.toString(paramValue);
+			}
+			
+			public String getParameterToken() {
+				return tokenValue;
+			}
+			
+			public InternationalizableTerm getParameterName() {
+				return new InternationalizableTerm("testing parameter");
+			}
+			
+			public InternationalizableTerm getParameterDescription() {
+				return new InternationalizableTerm("An integer testing parameter");
+			}
+		}
+		DataParameter _param = new TestDataParameter("param1") ;
+		DataParameter _param_2 = new TestDataParameter("param2");
+
+		_data.addDataParameter(_param);
+		_data.addDataParameter(_param);
+		_data.addDataParameter(_param_2);
+		
+		_data.addSubData(_child_1);
+		_data.addSubData(_child_1);
+		_data.addSubData(_child_2);
+		
+		_child_1.addDataParameter(_param);
+		_child_2.addDataParameter(_param_2);
+		
+		class TwoInt{
+			public int nbVisitedParam;
+			public int nbVisitedNode;
+			public TwoInt() {
+				nbVisitedNode = 0;
+				nbVisitedParam = 0;
+			}
+		};
+		
+		final TwoInt two_int = new TwoInt();
+		
+		
+		DataVisitor _visitor = new DataVisitor() {
+			
+			public void performTreatmentOn(DataParameter data_param) {
+				two_int.nbVisitedParam ++;
+			}
+			
+			public void performTreatmentOn(HierarchicalData data_node) {
+				two_int.nbVisitedNode ++;
+			}
+		};
+		
+		_data.acceptVisitor(_visitor);
+		assertTrue(two_int.nbVisitedNode == 3);
+		assertTrue(two_int.nbVisitedParam == 4);
+		
+	}
+
+}
