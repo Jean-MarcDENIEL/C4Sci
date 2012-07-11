981b1b0d21d7db5896384978acf7c02e4951c69b
JeanMarcDENIEL <jean-marc.deniel@inrs.fr>
2012-07-11 17:14:04 +0200

    BooleanDataParameter class created and tested

---
+++ C4Sci/src/main/java/c4sci/data/BooleanDataParameter.java
@@ -0,0 +1,44 @@
+
+package c4sci.data;
+
+import c4sci.data.internationalization.InternationalizableTerm;
+
+/**
+ * @author jeanmarc.deniel
+ *
+ */
+public class BooleanDataParameter extends DataParameter {
+
+	private boolean paramValue;
+	
+	/**
+	 * Constructs a BooleanDataParaeter whose value is false 
+	 */
+	public BooleanDataParameter(String token_str,
+			InternationalizableTerm name_term,
+			InternationalizableTerm descr_term) {
+		super(token_str, name_term, descr_term);
+		paramValue = false;
+	}
+
+
+	@Override
+	public String getParameterValue() {
+		return Boolean.toString(paramValue);
+	}
+
+
+	@Override
+	public void setParameterValue(String str_to_parse)
+			throws DataValueParsingException {
+		paramValue = Boolean.parseBoolean(str_to_parse);
+	}
+	
+	public void setBooleanValue(final boolean b_val){
+		paramValue = b_val;
+	}
+	public boolean getBooleanValue(){
+		return paramValue;
+	}
+
+}
---
+++ C4Sci/src/test/java/c4sci/data/TestBooleanParameter.java
@@ -0,0 +1,43 @@
+package c4sci.data;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import c4sci.data.internationalization.InternationalizableTerm;
+
+public class TestBooleanParameter {
+
+	@Test
+	public void test() {
+		BooleanDataParameter _param = new BooleanDataParameter("param", 
+				new InternationalizableTerm("boolean param"), 
+				new InternationalizableTerm("param descr"));
+		
+		try {
+			_param.setParameterValue("true");
+		} catch (DataValueParsingException e) {
+			fail();
+		}
+		assertTrue(_param.getBooleanValue());
+		
+		try {
+			_param.setParameterValue("kj");
+		} catch (DataValueParsingException e) {
+			fail();
+		}
+		assertFalse(_param.getBooleanValue());
+		
+		_param.setBooleanValue(true);
+		try {
+			_param.setParameterValue(null);
+		} catch (DataValueParsingException e) {
+			fail();
+		}
+		assertFalse(_param.getBooleanValue());
+		
+		assertTrue("false".compareTo(_param.getParameterValue())==0);
+		
+	}
+
+}
