cb72f292a97cf6d8c5d253085968987a3ed35446
JeanMarcDENIEL <jean-marc.deniel@inrs.fr>
2012-07-20 14:54:03 +0200

    more explanatory map name

--- C4Sci/src/main/java/c4sci/data/internationalization/Languages.java
+++ C4Sci/src/main/java/c4sci/data/internationalization/Languages.java
@@ -5,7 +5,7 @@ import java.util.concurrent.ConcurrentHashMap;
 
 public final class Languages {
 	private Languages(){}
-	private static Map<String, Language>BASIC_LANGUAGES_MAP = createBasicLanguages();
+	private static Map<String, Language>LANGUAGES_BASIC_MAP = createBasicLanguages();
 	
 	private static Map<String, Language> createBasicLanguages(){
 		Map<String, Language> _res = new ConcurrentHashMap<String, Language>();
@@ -17,7 +17,7 @@ public final class Languages {
 
 
 	public static void addNewLanguage(Language l_to_add){
-		BASIC_LANGUAGES_MAP.put(l_to_add.getLanguageSymbol(), l_to_add);
+		LANGUAGES_BASIC_MAP.put(l_to_add.getLanguageSymbol(), l_to_add);
 	}
 	
 	/**
@@ -26,9 +26,9 @@ public final class Languages {
 	 * @throws NoCorrespondingLanguageException if there is no correspondence between lang_symbol and a known language
 	 */
 	public static Language getLanguage(String lang_symbol) throws NoCorrespondingLanguageException{
-		if (!BASIC_LANGUAGES_MAP.containsKey(lang_symbol)){
+		if (!LANGUAGES_BASIC_MAP.containsKey(lang_symbol)){
 			throw new NoCorrespondingLanguageException(lang_symbol, "No corresponding language!");
 		}
-		return BASIC_LANGUAGES_MAP.get(lang_symbol);
+		return LANGUAGES_BASIC_MAP.get(lang_symbol);
 	}
 }
