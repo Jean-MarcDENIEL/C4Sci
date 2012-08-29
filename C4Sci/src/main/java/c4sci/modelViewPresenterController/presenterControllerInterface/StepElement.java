57fbe13fbcbb0891860b541df5ad912f3d142630
JeanMarcDENIEL <jean-marc.deniel@inrs.fr>
2012-08-29 15:22:23 +0200

    presenterControllerInterface begun.

---
+++ C4Sci/src/main/java/c4sci/modelViewPresenterController/presenterControllerInterface/StepElement.java
@@ -0,0 +1,12 @@
+package c4sci.modelViewPresenterController.presenterControllerInterface;
+/**
+ * StepElements are elements of the application that are concerned at a certain application step.
+ * @author jeanmarc.deniel
+ *
+ */
+public abstract class StepElement {
+
+	public StepElement() {
+	}
+
+}
