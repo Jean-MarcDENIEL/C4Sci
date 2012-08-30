356bb30c5b79f9e2886e1c633810cc6b4ce6fb00
JeanMarcDENIEL <jean-marc.deniel@inrs.fr>
2012-08-30 11:39:22 +0200

    Unit scales fully tested

--- C4Sci/src/test/java/c4sci/modelViewPresenterController/presenterControllerInterface/TestUnitScales.java
+++ C4Sci/src/test/java/c4sci/modelViewPresenterController/presenterControllerInterface/TestUnitScales.java
@@ -8,6 +8,7 @@ import c4sci.data.internationalization.InternationalizableTerm;
 
 public class TestUnitScales {
 
+	@SuppressWarnings("unused")
 	@Test
 	public void test() {
 
@@ -16,6 +17,7 @@ public class TestUnitScales {
 		_unit.addScale("km", new InternationalizableTerm("kilometer"),1000.0f);
 		_unit.addScale("dm", new InternationalizableTerm("decimeter"),0.1f);
 		_unit.addScale("dam", new InternationalizableTerm("decameter"), 10f);
+		_unit.addScale("falseScale", new InternationalizableTerm("false scale"), 0.0f);
 		
 		String[] _abbrev_tab = _unit.getSortedAbbreviations();
 		assertTrue(_abbrev_tab.length == 5);
@@ -46,6 +48,58 @@ public class TestUnitScales {
 		catch(NoSuchScaleExistsException _e){
 			assertTrue(true);
 		}
+	}
+	
+	@Test
+	public void testGetScalesAndAbbreviations(){
+		UnitScales _unit = new UnitScales("m", new InternationalizableTerm("meter"));
+		_unit.addScale("mm",new InternationalizableTerm("millimeter"),0.001f);
+		_unit.addScale("km", new InternationalizableTerm("kilometer"),1000.0f);
+		_unit.addScale("dm", new InternationalizableTerm("decimeter"),0.1f);
+		_unit.addScale("dam", new InternationalizableTerm("decameter"), 10f);
+		String[] _abbrev_tab = _unit.getSortedAbbreviations();
+		
+		for (String _abbrev : _abbrev_tab){
+			try {
+				float _scale = _unit.getScalingFactor(_abbrev);
+				String _abbrev_2 = _unit.getAbbreviation(_scale);
+				assertTrue(_abbrev_2.compareTo(_abbrev)==0);
+			} catch (NoSuchScaleExistsException _e) {
+				fail("should not throw here : "+_e.getMessage());
+			}
+		}
+		String _to_mm = _unit.chooseBestFittedScale(0.001f);
+		String _to_cm = _unit.chooseBestFittedScale(0.035f);
+		String _to_km = _unit.chooseBestFittedScale(990.0f);
+		String _to_dam = _unit.chooseBestFittedScale(45f);
+		String _to_m = _unit.chooseBestFittedScale(3.1f);
+		String _to_0 = _unit.chooseBestFittedScale(0.0f);
+		assertTrue("instead of mm :"+_to_mm, 	"mm".compareTo(_to_mm)==0);
+		assertTrue("instead of dm :"+_to_cm, 	"dm".compareTo(_to_cm)==0);
+		assertTrue("instead of km :"+_to_km, 	"km".compareTo(_to_km)==0);
+		assertTrue("instead of dam:"+_to_dam, 	"dam".compareTo(_to_dam)==0);
+		assertTrue("instead of m : "+_to_m, 	"m".compareTo(_to_m)==0);
+		assertTrue("instead of mm :"+_to_0,		"mm".compareTo(_to_0)==0);
+	}
+	
+	@Test
+	public void TestCreateMeterScale(){
+		UnitScales _unit= UnitScales.createMeterUnitSCales();
+		String _to_mm = _unit.chooseBestFittedScale(0.001f);
+		String _to_cm = _unit.chooseBestFittedScale(0.01f);
+		String _to_dm = _unit.chooseBestFittedScale(0.1f);
+		String _to_km = _unit.chooseBestFittedScale(1000f);
+		String _to_dam = _unit.chooseBestFittedScale(10f);
+		String _to_m = _unit.chooseBestFittedScale(1f);
+		String _to_0 = _unit.chooseBestFittedScale(0.0f);
+		
+		assertTrue("instead of mm :"+_to_mm, 	"mm".compareTo(_to_mm)==0);
+		assertTrue("instead of cm :"+_to_cm, 	"cm".compareTo(_to_cm)==0);
+		assertTrue("instead of dm :"+_to_dm, 	"dm".compareTo(_to_dm)==0);
+		assertTrue("instead of km :"+_to_km, 	"km".compareTo(_to_km)==0);
+		assertTrue("instead of dam:"+_to_dam, 	"dam".compareTo(_to_dam)==0);
+		assertTrue("instead of m : "+_to_m, 	"m".compareTo(_to_m)==0);
+		assertTrue("instead of mm :"+_to_0,		"mm".compareTo(_to_0)==0);
 	}
 
 
