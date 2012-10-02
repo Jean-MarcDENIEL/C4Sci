33538e7e8a311f12b5d0e163eff6cfb9178683d0
JeanMarcDENIEL <jean-marc.deniel@inrs.fr>
2012-10-02 10:10:27 +0200

    Refactoring continued

--- C4Sci/src/main/java/c4sci/modelViewPresenterController/jobs/changes/ChangeRunnablePrototype.java
+++ C4Sci/src/main/java/c4sci/modelViewPresenterController/jobs/changes/ChangeRunnablePrototype.java
@@ -16,7 +16,12 @@ public abstract class ChangeRunnablePrototype<C extends Command> implements Runn
 	public ChangeRunnablePrototype(C change_command){
 		setChangeCommand(change_command);
 	}
-	
+	/**
+	 * Creates a {@link ChangeRunnablePrototype} of the same type of "this".<br>
+	 * <b>Pattern :</b> This method instantiates the <b>Prototype</b> GoF pattern.
+	 * @param change_command The {@link Command} the return value should work on.
+	 * @return <i>null</i> if cannot create the clone adapted to the parameter.
+	 */	
 	abstract public ChangeRunnablePrototype<C> getClone(C change_command);
 
 	public final C getChangeCommand() {
@@ -26,7 +31,11 @@ public abstract class ChangeRunnablePrototype<C extends Command> implements Runn
 	public final void setChangeCommand(C changeCommand) {
 		this.changeCommand = changeCommand;
 	}
-	
+	/**
+	 * The method to call in order to get the changes performed on the {@link Command}.
+	 */
+	//CHECKSTYLE:OFF
 	public abstract void run();
+	//CHECKSTYLE:ON
 
 }
