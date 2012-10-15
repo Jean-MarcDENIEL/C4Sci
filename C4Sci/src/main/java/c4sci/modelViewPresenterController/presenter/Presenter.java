25a3d0bad43a46236aafeca795a53ab40cf48721
JeanMarcDENIEL <jean-marc.deniel@inrs.fr>
2012-10-15 15:03:18 +0200

    Presenter continued

--- C4Sci/src/main/java/c4sci/modelViewPresenterController/presenter/Presenter.java
+++ C4Sci/src/main/java/c4sci/modelViewPresenterController/presenter/Presenter.java
@@ -4,49 +4,82 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.NoSuchElementException;
 import java.util.concurrent.ConcurrentHashMap;
 
 import c4sci.data.DataIdentity;
 import c4sci.data.internationalization.InternationalizableTerm;
 import c4sci.modelViewPresenterController.MvpcLayer;
+import c4sci.modelViewPresenterController.jobs.Command;
 import c4sci.modelViewPresenterController.jobs.JobProcessor;
 import c4sci.modelViewPresenterController.jobs.JobProcessorFactory;
 import c4sci.modelViewPresenterController.presenterControllerInterface.StepChange;
 import c4sci.modelViewPresenterController.presenterControllerInterface.StepElement;
 import c4sci.modelViewPresenterController.presenterControllerInterface.stepChanges.ElementActivatedStepChange;
 import c4sci.modelViewPresenterController.presenterControllerInterface.stepChanges.ElementAddedStepChange;
-import c4sci.modelViewPresenterController.presenterControllerInterface.stepChanges.ElementFeedbackModificationStepChange;
+import c4sci.modelViewPresenterController.presenterControllerInterface.stepChanges.ElementValueStepChange;
 import c4sci.modelViewPresenterController.presenterControllerInterface.stepChanges.ElementInactivatedStepChange;
 import c4sci.modelViewPresenterController.presenterControllerInterface.stepChanges.ElementReactiveModificationStepChange;
 import c4sci.modelViewPresenterController.presenterControllerInterface.stepChanges.ElementSuppressedStepChange;
 import c4sci.modelViewPresenterController.presenterControllerInterface.stepChanges.StepBackwardStepChange;
 import c4sci.modelViewPresenterController.presenterControllerInterface.stepChanges.StepForwardStepChange;
+import c4sci.modelViewPresenterController.presenterControllerInterface.stepChanges.ElementValueChanges.BooleanElementModificationStepChange;
+import c4sci.modelViewPresenterController.presenterControllerInterface.stepChanges.ElementValueChanges.FloatElementModificationStepChange;
+import c4sci.modelViewPresenterController.presenterControllerInterface.stepChanges.ElementValueChanges.IntegerElementModificationStepChange;
+import c4sci.modelViewPresenterController.presenterControllerInterface.stepChanges.ElementValueChanges.InternationalizableTermElementModificationStepChange;
+import c4sci.modelViewPresenterController.presenterControllerInterface.stepChanges.ElementValueChanges.ThreeDimensionalElementModificationStepChange;
+import c4sci.modelViewPresenterController.presenterControllerInterface.stepChanges.ElementValueChanges.TwoDimensionalElementModificationStepChange;
 import c4sci.modelViewPresenterController.presenterControllerInterface.stepElements.dataParameterDataElements.BooleanDataElement;
 import c4sci.modelViewPresenterController.presenterControllerInterface.stepElements.dataParameterDataElements.FloatDataElement;
 import c4sci.modelViewPresenterController.presenterControllerInterface.stepElements.dataParameterDataElements.IntegerDataElement;
 import c4sci.modelViewPresenterController.presenterControllerInterface.stepElements.dataParameterDataElements.LabelDataElement;
 import c4sci.modelViewPresenterController.viewerPresenterInterface.Component;
 import c4sci.modelViewPresenterController.viewerPresenterInterface.ComponentChange;
+import c4sci.modelViewPresenterController.viewerPresenterInterface.ComponentFamily;
+import c4sci.modelViewPresenterController.viewerPresenterInterface.ComponentFamily.StandardComponentSet;
+import c4sci.modelViewPresenterController.viewerPresenterInterface.componentChanges.generics.TwoDimensionalChange;
 import c4sci.modelViewPresenterController.viewerPresenterInterface.componentChanges.modificationChanges.BooleanValueChange;
 import c4sci.modelViewPresenterController.viewerPresenterInterface.componentChanges.modificationChanges.FloatValueChange;
 import c4sci.modelViewPresenterController.viewerPresenterInterface.componentChanges.modificationChanges.IntegerValueChange;
 import c4sci.modelViewPresenterController.viewerPresenterInterface.componentChanges.modificationChanges.LabelChange;
+import c4sci.modelViewPresenterController.viewerPresenterInterface.componentChanges.modificationChanges.StringValueChange;
 
 public abstract class Presenter implements MvpcLayer<ComponentChange, StepChange>{
 
 	private Map<Component, StepElement> componentElementMap;
 	private Map<StepElement, Component> elementComponentMap;
 
-	
+
 	public Presenter(){
 		componentElementMap = new ConcurrentHashMap<Component, StepElement>();
 		elementComponentMap	= new ConcurrentHashMap<StepElement, Component>();
 	}
 
 
+	public Component getCorrespondingComponent(StepElement step_elt) throws NoSuchElementException{
+		Component _res= elementComponentMap.get(step_elt);
+		if (_res == null){
+			throw new NoSuchElementException();
+		}
+		return _res;
+	}
+	
+	public void setCorrespondence(Component comp_, StepElement step_elt){
+		componentElementMap.put(comp_, step_elt);
+		elementComponentMap.put(step_elt, comp_);
+	}
+
+	public StepElement getCorrespondingElement(Component comp_) throws NoSuchElementException{
+		StepElement _res = componentElementMap.get(comp_);
+		if (_res == null){
+			throw new NoSuchElementException();
+		}
+		return _res;
+	}
+
 	public JobProcessorFactory<ComponentChange, StepChange> getReactiveJobProcessorFactory() {
 		JobProcessorFactory<ComponentChange, StepChange> 	_reactive_job_processor_factory = new JobProcessorFactory<ComponentChange, StepChange>();
-		
+
 		_reactive_job_processor_factory.addChangePerformingAbility(BooleanValueChange.class, new JobProcessor<ComponentChange, StepChange>() {
 			@Override
 			public List<StepChange> processJob(ComponentChange processing_cmd) {
@@ -65,16 +98,16 @@ public abstract class Presenter implements MvpcLayer<ComponentChange, StepChange
 				return reactionToIntegerValueChange(processing_cmd);
 			}
 		});
-		
+
 		return _reactive_job_processor_factory;
 	}
 
 	public JobProcessorFactory<StepChange, ComponentChange> getFeedbackJobProcessorFactory() {
 		JobProcessorFactory<StepChange, ComponentChange> _feedback_job_processor_factory = new JobProcessorFactory<StepChange, ComponentChange>();
-		
+
 		return _feedback_job_processor_factory;
 	}
-	
+
 	/********************* FEEDBACK METHODS ***************************************************/
 	public abstract List<ComponentChange> feedbackToElementActivatedStepChange(ElementActivatedStepChange step_chg);
 	public abstract List<ComponentChange> feedbackToElementAddedStepChange(ElementAddedStepChange step_chg);
@@ -82,92 +115,28 @@ public abstract class Presenter implements MvpcLayer<ComponentChange, StepChange
 	public abstract List<ComponentChange> feedbackToStepBackwardStepChange(StepBackwardStepChange step_chg);
 	public abstract List<ComponentChange> feedbackToStepForwardStepChange(StepForwardStepChange step_chg);
 	public abstract List<ComponentChange> feedbackToElementInactivatedStepChange(ElementInactivatedStepChange step_chg);
-	public abstract List<ComponentChange> feedbackToElementFeedbackModificationStepChange(ElementFeedbackModificationStepChange step_chg);
-	/**
-	 * Feedback to an element having been modified :
-	 * <ul>
-	 * 	<li>BooleanDataElement
-	 * </ul>. <br>
-	 * This method contains the default behavior and can be overridden. 
-	 * @param step_chg
-	 * @return
-	 */
-	public List<ComponentChange> feedbackToElementFeedbackModificationStepChange(StepChange step_chg){
-		Component _corresp_comp = elementComponentMap.get(step_chg.getStepElement());
-		List<ComponentChange> _feedback_comp_chg = null;
-		if (_corresp_comp != null){
-			_feedback_comp_chg = new ArrayList<ComponentChange>();	
-			computeDefaultFeedback(_corresp_comp, step_chg.getStepElement(), _feedback_comp_chg);
-		}
-		return _feedback_comp_chg;
-	}
-	
+	public abstract List<ComponentChange> feedbackToBooleanElementModificationStepChange(BooleanElementModificationStepChange step_chg);
+	public abstract List<ComponentChange> feedbackToFloatElementModificationStepChange(FloatElementModificationStepChange step_chg);
+	public abstract List<ComponentChange> feedbackToIntegerElementModificationStepChange(IntegerElementModificationStepChange step_chg);
+	public abstract List<ComponentChange> feedbackToInternationalizableTermElementModificationStepChange(InternationalizableTermElementModificationStepChange step_chg);
+	public abstract List<ComponentChange> feedbackToThreeDimensionalElementModificationStepChange(ThreeDimensionalElementModificationStepChange step_chg);
+	public abstract List<ComponentChange> feedbackToTwoDimensionElementModificationStepChange(TwoDimensionalElementModificationStepChange step_chg);
+
 	/********************* REACTION METHODS **************************************************/
-	
+	public abstract List<StepChange> reactionToBooleanValueChange(BooleanValueChange comp_chg);
+	public abstract List<StepChange> reactionToFloatValueChange(FloatValueChange comp_chg);
+	public abstract List<StepChange> reactionToIntegerValueChange(IntegerValueChange comp_chg);
+	public abstract List<StepChange> reactionToStringValueChange(StringValueChange comp_chg);
+
+	/********************* FACTORY METHODS ***************************************************/
 	/**
-	 * Reaction to a boolean value change.<br>
-	 * Default behavior is passing it to the "feedback side". It can be overridden to achieve special behavior.
-	 * @param cmd_chg the {@link Command} indicating the value has been changed.
-	 * @return The corresponding {@link StepChange} list.
+	 * 
+	 * Computes the {@link Command} sequence corresponding to the creation of the {@link StepElement}.<br>
+	 * 
+	 * @param corresp_component the {@link Component} corresponding to the {@link StepElement} argument.
+	 * @param step_elt The {@link StepElement} to analyze.
+	 * @param previous_change The {@link Command} that is previous all {@link Command commands} of the result
+	 * @return a list of {@link ComponentChange}
 	 */
-	public List<StepChange> reactionToBooleanValueChange(ComponentChange cmd_chg){
-		final String _str_val = Boolean.toString(((BooleanValueChange)cmd_chg).getChange());
-		return oneElementReactiveNotificationChange(cmd_chg, _str_val);
-	}
-	/**
-	 * Reaction to a float value change.<br>
-	 * Default behavior is passing it to the "feedback side". It can be overridden to achieve special behavior.
-	 * @param cmd_chg the {@link Command} indicating the value has been changed.
-	 * @return The corresponding {@link StepChange} list.
-	 */
-	public List<StepChange> reactionToFloatValueChange(ComponentChange cmd_chg){
-		final String _float_str = Float.toString(((FloatValueChange)cmd_chg).getChange());
-		return oneElementReactiveNotificationChange(cmd_chg, _float_str);
-	}
-	/**
-	 * Reaction to a boolean value change.<br>
-	 * Default behavior is passing it to the "feedback side". It can be overridden to achieve special behavior.
-	 * @param comp_chg the {@link Command} indicating the value has been changed.
-	 * @return The corresponding {@link StepChange} list.
-	 */
-	public List<StepChange> reactionToIntegerValueChange(ComponentChange comp_chg){
-		final String _int_str = Integer.toString(((IntegerValueChange)comp_chg).getChange());
-		return oneElementReactiveNotificationChange(comp_chg, _int_str);
-	}
-	
-	
-	private List<StepChange> oneElementReactiveNotificationChange(ComponentChange comp_chg, String value_str){
-		List<StepChange> _res = new ArrayList<StepChange>();
-		StepElement _corresp_element = componentElementMap.get(comp_chg.getComponentIdentity());
-		if (_corresp_element != null){
-			_res.add(new ElementReactiveModificationStepChange(null, _corresp_element, value_str));
-		}
-		return _res;
-	}
-	
-	private void computeDefaultFeedback(Component comp_elt, StepElement step_elt, List<ComponentChange> res_list){
-		if (step_elt.containsProperValue()){
-			String _elt_value = step_elt.getProperValue();
-			if (step_elt instanceof BooleanDataElement){
-				res_list.add(new BooleanValueChange(comp_elt, Boolean.parseBoolean(_elt_value), null));
-			}
-			if (step_elt instanceof FloatDataElement){
-				res_list.add(new FloatValueChange(comp_elt, Float.parseFloat(_elt_value), null));
-			}
-			if (step_elt instanceof IntegerDataElement){
-				res_list.add(new IntegerValueChange(comp_elt, Integer.parseInt(_elt_value), null));
-			}
-			if (step_elt instanceof LabelDataElement){
-				res_list.add(new LabelChange(comp_elt, new InternationalizableTerm(_elt_value), null));
-			}
-		}
-		Iterator<StepElement> _it = step_elt.getSubElementsIterator();
-		while (_it.hasNext()){
-			StepElement _sub_elt = _it.next();
-			Component _sub_id = elementComponentMap.get(_sub_elt);
-			if (_sub_id != null){
-				computeDefaultFeedback(_sub_id, _sub_elt, res_list);
-			}
-		}
-	}
+	public abstract List<ComponentChange> computeCreationSequence(Component corresp_component, StepElement step_elt, ComponentChange previous_change);
 }
--- C4Sci/src/main/java/c4sci/modelViewPresenterController/presenterControllerInterface/stepChanges/ElementValueStepChange.java
+++ C4Sci/src/main/java/c4sci/modelViewPresenterController/presenterControllerInterface/stepChanges/ElementValueStepChange.java
@@ -4,9 +4,9 @@ import c4sci.modelViewPresenterController.jobs.Command;
 import c4sci.modelViewPresenterController.presenterControllerInterface.StepChange;
 import c4sci.modelViewPresenterController.presenterControllerInterface.StepElement;
 
-public class ElementFeedbackModificationStepChange extends StepChange {
+public class ElementValueStepChange extends StepChange {
 
-	public ElementFeedbackModificationStepChange(Command parent_command, StepElement step_element) {
+	public ElementValueStepChange(Command parent_command, StepElement step_element) {
 		super(parent_command, step_element);
 	}
 
