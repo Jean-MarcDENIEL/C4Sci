2eaa464ad0b8f407fce82e21b07ac741e6026b83
PCPTELABO <jean-marc.deniel@inrs.fr>
2012-12-14 09:36:04 +0100

    DataIdentity parsing and oString methods added and tested

--- C4Sci/src/main/java/c4sci/data/DataIdentity.java
+++ C4Sci/src/main/java/c4sci/data/DataIdentity.java
@@ -2,6 +2,8 @@ package c4sci.data;
 
 import java.util.Date;
 
+import c4sci.data.exceptions.DataValueParsingException;
+
 /**
  * This class is used to identify and retrieve HierarchicalData through persistent values.<br>
  * Uniqueness of identity values is achieved through two values :
@@ -17,13 +19,13 @@ public class DataIdentity {
 
 	private long	timeStamp;
 	private long	countStamp;
-	
+
 	private static long	currentTimeStamp = new Date().getTime();
 	private static long	currentCountStamp = 0;
 	private static synchronized 	long createCountStamp(){
 		return currentCountStamp ++;
 	}
-	
+
 	/**
 	 * Creates a new Identity with unique time stamp and count stamp. 
 	 */
@@ -31,13 +33,13 @@ public class DataIdentity {
 		timeStamp = currentTimeStamp;
 		countStamp = createCountStamp();
 	}
-	
+
 	public long getTimeStamp(){
 		return timeStamp;
 	}
 	/**
 	 * <b>Warning : </b> This method can break the DataIdentity uniqueness assumption.
-	 * @param time_stamp the Identity timestamp.
+	 * @param time_stamp the Identity time stamp.
 	 */
 	public void setTimeStamp(long time_stamp){
 		timeStamp = time_stamp;
@@ -52,7 +54,7 @@ public class DataIdentity {
 	public void setCountStamp(long count_stamp){
 		countStamp = count_stamp;
 	}
-	
+
 	/**
 	 * @return true if obj_ if of DataIdentity type and the time stamp and the count stamp are identical. 
 	 */
@@ -70,9 +72,33 @@ public class DataIdentity {
 			return false;
 		}
 	}
-	
+
 	public int hashCode(){
 		return (int)(timeStamp+countStamp);
 	}
 
+	public String toString(){
+		return ""+Long.toString(getTimeStamp())+" "+Long.toString(getCountStamp());
+	}
+	/**
+	 * Converts a string to retrieve the identity values.
+	 * @param parsed_str the string containing the {@link DataIdentity} representation that is normally given by the {@link #toString()} method. 
+	 * @throws DataValueParsingException in the case the parameter cannot be successfully parsed.
+	 */
+	public void parseFromString(String parsed_str) throws DataValueParsingException{
+		if (parsed_str == null){
+			throw new DataValueParsingException("long long", "null", "null expression to parse", null);
+		}
+		String[] _substrings = parsed_str.split(" ");
+		if (_substrings.length != 2){
+			throw new DataValueParsingException("long long", parsed_str, "should be composed of two longs", null);
+		}
+		try{
+			setTimeStamp(Long.parseLong(_substrings[0]));
+			setCountStamp(Long.parseLong(_substrings[1]));
+		}
+		catch(NumberFormatException _e){
+			throw new DataValueParsingException("two longs", parsed_str, "should be composed of two longs", _e);
+		}
+	}
 }
--- C4Sci/src/test/java/c4sci/data/TestDataIdentity.java
+++ C4Sci/src/test/java/c4sci/data/TestDataIdentity.java
@@ -4,27 +4,75 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import c4sci.data.exceptions.DataValueParsingException;
+
 public class TestDataIdentity {
 
 	@Test
 	public void testEqualsObject() {
 		DataIdentity _id_1 = new DataIdentity();
-		
+
 		DataIdentity _id_2 = new DataIdentity();
-		
+
 		DataIdentity _id_3 = new DataIdentity();
 		_id_3.setCountStamp(_id_1.getCountStamp());
 		_id_3.setTimeStamp(_id_1.getTimeStamp());
-		
+
 		assertFalse(_id_1.equals(new Integer(20)));
 		assertTrue(_id_1.equals(_id_1));
 		assertFalse(_id_1.equals(_id_2));
 		assertTrue(_id_1.equals(_id_3));
-		
+
 		assertTrue(_id_1.hashCode() == _id_1.hashCode());
 		assertFalse(_id_1.hashCode() == _id_2.hashCode());
 		assertTrue(_id_1.hashCode() == _id_3.hashCode());
-				
+
 	}
 
+	@Test
+	public void testParsingAndtString(){
+		DataIdentity _id_1 = new DataIdentity();
+		DataIdentity _id_2 = new DataIdentity();
+		assertFalse(_id_2.equals(_id_1));
+		try{
+			_id_2.parseFromString(_id_1.toString());
+		}
+		catch(DataValueParsingException _e){
+			fail("should not throw here");
+		}
+		assertTrue(_id_2.equals(_id_1));
+		assertTrue(_id_1.equals(_id_2));
+
+		try {
+			_id_2.parseFromString("21221 2545");
+		} catch (DataValueParsingException e) {
+			fail("should not throw here");
+		}
+		assertFalse(_id_2.equals(_id_1));
+
+		for (int _i=0; _i<5; _i++){
+			try {
+				switch(_i){
+				case 0:
+					_id_2.parseFromString("kj 120");
+					break;
+				case 1:
+					_id_2.parseFromString("120");
+					break;
+				case 2:
+					_id_2.parseFromString(null);
+					break;
+				case 3:
+					_id_2.parseFromString("120.0 240");
+					break;
+				case 4:
+					_id_2.parseFromString("120 240.0");
+				}
+				fail("should have thrown here");
+			} catch (DataValueParsingException e) {
+				assertTrue(true);
+			}
+		}
+	}
 }
+
