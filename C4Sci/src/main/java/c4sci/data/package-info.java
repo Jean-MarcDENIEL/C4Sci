725e6a9be3158a43681a5d285ff1e833181b06ad
PCPTELABO <jean-marc.deniel@inrs.fr>
2013-05-30 11:30:53 +0200

    Added class diagram and package documentation.

---
+++ C4Sci/src/main/java/c4sci/data/package-info.java
@@ -0,0 +1,8 @@
+package c4sci.data;
+
+/**
+ * This package is intended at giving useful data management abilities.
+ * 
+ * The class diagram is the following :
+ * <img src="doc-files/classes-diagram.png" width="100%">
+*/
