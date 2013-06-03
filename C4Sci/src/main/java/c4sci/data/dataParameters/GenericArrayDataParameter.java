94ba6012b1830738f8791cccc93244274bf62731
PCPTELABO <jean-marc.deniel@inrs.fr>
2013-06-03 16:30:44 +0200

    Testing of generic arrays of Modifiables begins

--- C4Sci/src/main/java/c4sci/data/dataParameters/GenericArrayDataParameter.java
+++ C4Sci/src/main/java/c4sci/data/dataParameters/GenericArrayDataParameter.java
@@ -2,6 +2,7 @@ package c4sci.data.dataParameters;
 
 import java.lang.reflect.Array;
 import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
 import java.util.regex.Pattern;
 
 import c4sci.data.DataParameter;
@@ -18,10 +19,13 @@ import c4sci.data.internationalization.InternationalizableTerm;
  */
 public class GenericArrayDataParameter<M extends Modifiable> extends DataParameter {
 
-	private static final String		INTEGER_REGEXP = "(\\+|-)?\\d+" ;
+	private static final String		INTEGER_REGEXP 		= "(\\+|-)?\\d+" ;
+	private static final String		WHITESPACE_REGEXP	= "\\s";
+	private static final String		WHITESPACE_SEQ		= " ";
 	
 	
-	private M[]		innerArray;
+	private M[]			innerArray;
+	private Class<M>	supportedClass;
 	
 	/**
 	 * Creates the parameter array and ensures that the array has no null element.
@@ -31,23 +35,23 @@ public class GenericArrayDataParameter<M extends Modifiable> extends DataParamet
 	 * @param descr_term
 	 * @throws CannotInstantiateParameterException
 	 */
+	@SuppressWarnings("unchecked")
 	public GenericArrayDataParameter(M[] inner_value, 
 			String token_str,
 			InternationalizableTerm name_term,
 			InternationalizableTerm descr_term) throws CannotInstantiateParameterException {
 		super(token_str, name_term, descr_term);
 		innerArray = inner_value;
-		
-		Class<M> _m_type = getSupportedClass();
+		supportedClass	= (Class<M>) inner_value.getClass().getComponentType();
 		
 		for (int _i=0; _i<innerArray.length; _i++){
 			if (innerArray[_i] == null){
 				try {
-					innerArray[_i] = _m_type.newInstance();
+					innerArray[_i] = getSupportedClass().newInstance();
 				} catch (InstantiationException  _e) {
-					throw new CannotInstantiateParameterException(token_str, "Cannot instantiate " + _m_type.getName(), _e);
+					throw new CannotInstantiateParameterException(token_str, "Cannot instantiate " + getSupportedClass().getName(), _e);
 				} catch (IllegalAccessException _e) {
-					throw new CannotInstantiateParameterException(token_str, "Cannot instantiate " + _m_type.getName(), _e);
+					throw new CannotInstantiateParameterException(token_str, "Cannot instantiate " + getSupportedClass().getName(), _e);
 				}
 			}
 		}
@@ -59,7 +63,7 @@ public class GenericArrayDataParameter<M extends Modifiable> extends DataParamet
 	@SuppressWarnings("unchecked")
 	@Override
 	protected DataParameter getSameDataParameterInstance() throws CannotInstantiateParameterException{
-		return new GenericArrayDataParameter<M>((M[])Array.newInstance(innerArray.getClass(), innerArray.length), 
+		return new GenericArrayDataParameter<M>((M[])Array.newInstance(getSupportedClass(), innerArray.length), 
 				getParameterToken(), getParameterName(), getParameterDescription());
 	}
 
@@ -68,7 +72,7 @@ public class GenericArrayDataParameter<M extends Modifiable> extends DataParamet
 
 		String _res = "" + innerArray.length;
 		for (M _element : innerArray){
-			_res = " " + _element.getValue();
+			_res += WHITESPACE_SEQ + _element.getValue();
 		}
 		return _res;
 	}
@@ -77,22 +81,25 @@ public class GenericArrayDataParameter<M extends Modifiable> extends DataParamet
 	@Override
 	public void setValue(String str_to_parse) throws DataValueParsingException {
 		try{
-			int _read_index = Pattern.compile(INTEGER_REGEXP).matcher(str_to_parse).end();
-			int _array_length = Integer.parseInt(str_to_parse.substring(0, _read_index));
 			
-			String _element_regexp = getSupportedClass().newInstance().getRegExp();
+			Pattern _whitespace_pattern = Pattern.compile(WHITESPACE_REGEXP);
+			String[] _sequences_tab = _whitespace_pattern.split(str_to_parse, 2);
 			
+			int _array_length = Integer.parseInt(_sequences_tab[0]);
 			if (innerArray.length != _array_length){
 				innerArray = (M[]) Array.newInstance(getSupportedClass(), _array_length);
 				for (int _i=0; _i<innerArray.length; _i++){
 					innerArray[_i] = getSupportedClass().newInstance();
 				}
 			}
-			String _read_str = str_to_parse; 
+
+			String _read_str = _sequences_tab[1]; 
 			for (int _i=0; _i<_array_length; _i++){
-				_read_str = _read_str.substring(_read_index);
-				innerArray[_i].setValue(_read_str);
-				_read_index = Pattern.compile(_element_regexp).matcher(_read_str).end();
+				_sequences_tab = _whitespace_pattern.split(_read_str, 2);
+				innerArray[_i].setValue(_sequences_tab[0]);
+				if (_sequences_tab.length>1){
+					_read_str = _sequences_tab[1];
+				}
 			}
 
 		}
@@ -113,7 +120,7 @@ public class GenericArrayDataParameter<M extends Modifiable> extends DataParamet
 			return "0";
 		}
 		else{
-			return INTEGER_REGEXP + " (" + innerArray[0].getRegExp() + "){"+ innerArray.length +"}";
+			return INTEGER_REGEXP + WHITESPACE_REGEXP + "(" + WHITESPACE_REGEXP + innerArray[0].getRegExp() + "){"+ innerArray.length +"}";
 		}
 	}
 	
@@ -125,9 +132,7 @@ public class GenericArrayDataParameter<M extends Modifiable> extends DataParamet
 		return innerArray;
 	}
 
-	@SuppressWarnings("unchecked")
 	private Class<M> getSupportedClass(){
-		ParameterizedType superClass = (ParameterizedType) getClass().getGenericSuperclass();
-		return (Class<M>) superClass.getActualTypeArguments()[0];
+		return supportedClass;
 	}
 }
